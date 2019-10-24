f00a2e493ccd2404a4b42653f4fe4f852043acb1
Eric Siu <siueric0010@gmail.com>
2019-10-24 01:10:21 -0700

    tested with user and id grabbbing, saved names and id's in private js file for now

--- bot2.js
+++ bot2.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 var auth = require('./auth.json');
+
 const prefix = '.'
 
 // Initialize Discord Bot
@@ -12,8 +13,19 @@ client.on('ready', () => {
 
 client.on('message', msg => {
   if (msg.content === `${prefix}server`) {
+    var keys = msg.guild.members.keyArray();
     msg.channel.send(`Server name: ${msg.guild.name} \nTotal members: ${msg.guild.memberCount}`);
-  }
+    msg.channel.send(`All Users In ${msg.guild.name}: `);
+    // msg.channel.send(user_and_id_1);
+    // msg.channel.send(user_and_id_2);
+    // console.log(msg.guild.members.get(msg.guild.members.firstKey()).user.username);
+  } else if  (msg.content === `${prefix}update`) {
+    /*var keys = msg.guild.members.keyArray();
+    for(var i = 0; i < keys.length; i++) {
+        user_and_id = user_and_id + msg.guild.members.get(keys[i]).user.username + " " + keys[i]+ "\n";
+    }
+*/    
+}
 });
 
 client.login(auth.token);
